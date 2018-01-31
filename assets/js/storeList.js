d1a93a448a507c1d170721eb99ba6509b27f53c4
Miller <millerliang@gmail.com>
2018-01-31 18:21:03 +0800

    add json data.js, fix img links

---
+++ assets/js/storeList.js
@@ -0,0 +1,326 @@
+var markersList = [
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "相機美人-敦化店",
+	"phone": "02-2711-0656",
+	"address": "台北市大安區敦化南路一段233巷48號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707311538080000001.jpg",
+	"loc": {
+		"lat": 25.0403231,
+		"lng": 121.55188429999998
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "相機美人-新光華店",
+	"phone": "02-2395-7277",
+	"address": "台北市中正區市民大道三段8號3樓45號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1709181323050000001.jpg",
+	"loc": {
+		"lat": 25.0450357,
+		"lng": 121.55188429999998
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "相機美人-南港中信園區店",
+	"phone": "02-2785-0155",
+	"address": "台北市南港區經貿二路186號C棟2樓",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1709181317490000001.jpg",
+	"loc": {
+		"lat": 25.0586561,
+		"lng": 121.61495089999994
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "弘采科技-新店家樂福店",
+	"phone": "02-8919-3138",
+	"address": "新北市新店區中興路三段1號2樓",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1709181329520000001.jpg",
+	"loc": {
+		"lat": 24.9761049,
+		"lng": 121.54659530000004
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "和信嘉-內壢家樂福店",
+	"phone": "03-463-8746",
+	"address": "桃園市中壢區中華路一段450號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1709181334440000001.jpg",
+	"loc": {
+		"lat": 24.9727564,
+		"lng": 121.25344799999993
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "極美職人",
+	"phone": "04-2301-3331",
+	"address": "台中市西區模範街80號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707171803320000001.jpg",
+	"loc": {
+		"lat": 24.15062,
+		"lng": 120.66673390000005
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "a life 生活家電旗艦館",
+	"phone": "04-2223-1873",
+	"address": "台中市北區三民路3段168巷1號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707171835190000001.jpg",
+	"loc": {
+		"lat": 24.1521983,
+		"lng": 120.6853509
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "世鈞科技",
+	"phone": "04-839-3708",
+	"address": "彰化縣員林市中山路二段468號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707171828000000001.jpg",
+	"loc": {
+		"lat": 23.9707539,
+		"lng": 120.57159100000001
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "羅森資訊-瑞豐生活館",
+	"phone": "07-522-8711",
+	"address": "高雄市鼓山區南屏路501號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707171944540000001.jpg",
+	"loc": {
+		"lat": 22.6654023,
+		"lng": 120.30021039999997
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "羅森資訊-金典綠園道",
+	"phone": "04-232-39155",
+	"address": "臺中市西區健行路1049號1F",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1710191318480000001.jpg",
+	"loc": {
+		"lat": 24.1557383,
+		"lng": 120.66326100000003
+	}
+},
+{
+	"url": "#0",
+	"type": "type1",
+	"title": "羅森資訊-鳳山生活館",
+	"phone": "07-780-6800",
+	"address": "高雄市鳳山區青年路二段457號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707171937060000001.jpg",
+	"loc": {
+		"lat": 22.644978,
+		"lng": 120.35010490000002
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "誠品信義-相機美人專櫃",
+	"phone": "02-8789-3388 #3115",
+	"address": "台北市信義區松高路11號2樓",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707311523300000001.jpg",
+	"loc": {
+		"lat": 25.039751,
+		"lng": 121.565870
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "統一時代百貨-明昱專櫃",
+	"phone": "0973-881-299",
+	"address": "台北市忠孝東路五段8號6樓",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707252237590000001.jpg",
+	"loc": {
+		"lat": 25.040846,
+		"lng": 121.565396
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "忠孝SOGO-怡眾專櫃",
+	"phone": "02-2752-5661",
+	"address": "台北市忠孝東路四段45號8樓",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707271131220000001.jpg",
+	"loc": {
+		"lat": 25.041870,
+		"lng": 121.544633
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "京站百貨-明昱專櫃",
+	"phone": "0986-021-626",
+	"address": "台北市大同區承德路一段1號3樓",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1710191946540000001.jpg",
+	"loc": {
+		"lat": 25.049308,
+		"lng": 121.517194
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "中和環球購物中心-明昱專櫃",
+	"phone": "02-8685-31556",
+	"address": "新北市中和區中山路三段122號B1",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707252346010000001.jpg",
+	"loc": {
+		"lat": 25.006673,
+		"lng": 121.474778
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "台茂購物中心-和信嘉專櫃",
+	"phone": "03-3521249",
+	"address": "桃園市蘆竹區南崁路一段112號5樓",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1710191328530000001.jpg",
+	"loc": {
+		"lat": 25.053142,
+		"lng": 121.287868
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "中壢SOGO-怡眾專櫃",
+	"phone": "03-427-6239",
+	"address": "中壢市元化路357號7樓",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1710191331260000001.jpg",
+	"loc": {
+		"lat": 24.962727,
+		"lng": 121.223824
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "台中NOVA-a life生活家電館",
+	"phone": "04-2326-3081",
+	"address": "台中市西區英才路508號1樓135櫃位",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707252312290000001.jpg",
+	"loc": {
+		"lat": 24.151455,
+		"lng": 120.665068
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "勤美誠品-鈞越專櫃",
+	"phone": "04-2328-1000#5010",
+	"address": "台中市公益路68號B1",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707252328340000001.JPG",
+	"loc": {
+		"lat": 24.151235,
+		"lng": 120.663848
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "嘉義耐斯百貨-相機美人專櫃",
+	"phone": "05-271-0097",
+	"address": "嘉義市東區忠孝路600號B1",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1710191325500000001.jpg",
+	"loc": {
+		"lat": 23.496520,
+		"lng": 120.452582
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "羅森資訊-台中大遠百誠品",
+	"phone": "(04)225-42855",
+	"address": "臺中市西屯區台灣大道三段251號9F",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707311529130000001.jpg",
+	"loc": {
+		"lat": 24.164863,
+		"lng": 120.644717
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "新光三越台南西門店-廣積專櫃",
+	"phone": "06-303-0999",
+	"address": "台南市中西區西門路一段658號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707252250200000001.jpg",
+	"loc": {
+		"lat": 22.986828,
+		"lng": 120.197703
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "漢神巨蛋-羅森專櫃",
+	"phone": "07-5866593",
+	"address": "高雄市左營區博愛路二段777號B1",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707211539410000001.jpg",
+	"loc": {
+		"lat": 22.669870,
+		"lng": 120.302537
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "羅森資訊-南紡購物中心",
+	"phone": "(06)208-1700",
+	"address": "台南市東區中華東路一段366號B1",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707271107540000001.jpg",
+	"loc": {
+		"lat": 22.990735,
+		"lng": 120.233097
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "羅森資訊-高雄夢時代百貨",
+	"phone": "(07)823-0055",
+	"address": "高雄市前鎮區中華五路789號3F",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707271121170000001.jpg",
+	"loc": {
+		"lat": 22.595045,
+		"lng": 120.307137
+	}
+},
+{
+	"url": "#0",
+	"type": "type2",
+	"title": "屏東太平洋百貨-羅森專櫃",
+	"phone": "08-732-5666",
+	"address": "屏東縣屏東市中正路72號",
+	"img": "http://www.chiconydcd.com.tw/upload/store/1707271055300000001.jpg",
+	"loc": {
+		"lat": 22.674243,
+		"lng": 120.489824
+	}
+}
+];
